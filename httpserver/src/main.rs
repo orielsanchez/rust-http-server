5dba7cf38ce7306597712ff3e588914848991f9d
Oriel Sanchez <orielbenitosanchez@gmail.com>
2024-11-03 19:46:18 -0800

    finished http server mvp

--- httpserver/src/main.rs
+++ httpserver/src/main.rs
@@ -1,3 +1,12 @@
+mod handler;
+mod router;
+mod server;
+
+use server::Server;
+
 fn main() {
-    println!("Hello, world!");
+    // Start a server
+    let server = Server::new("localhost:3000");
+    // Run server
+    server.run();
 }
